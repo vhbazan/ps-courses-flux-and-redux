de15bdd7fbfe7c60e17be4f59c3610cb9161a5ce
vbazanm <victor_bazan_milla@optum.com>
2020-06-22 12:49:58 +0200

    Add change font size action

--- src/control-panel.js
+++ src/control-panel.js
@@ -10,6 +10,13 @@ document
     controlPanelDispatcher.dispatch("TODO_NAMEINPUT_ACTION");
   });
 
+document.forms.fontSizeForm.fontSize.forEach(element => {
+  element.addEventListener("change", ({ target }) => {
+    console.log("Dispatching ...", target.value);
+    controlPanelDispatcher.dispatch("CHANGE_FONT_SIZE_ACTION");
+  });
+});
+
 controlPanelDispatcher.register(action => {
   console.info("action", action);
 });
