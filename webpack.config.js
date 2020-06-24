89ffef65f093dcb522bb93b5a2a4adeb795f8c55
vbazanm <victor_bazan_milla@optum.com>
2020-06-24 12:23:28 +0200

    Scafold task section

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,31 @@
-const path = require('path');
+const path = require("path");
 module.exports = {
-    module: {
-        loaders: [
-            {
-                loader: "babel-loader",
+  module: {
+    loaders: [
+      {
+        loader: "babel-loader",
 
-                include: [
-                    path.resolve(__dirname, "src"),
-                ],
+        include: [path.resolve(__dirname, "src")],
 
-                test: /\.js?$/,
+        test: /\.js?$/,
 
-                // Options to configure babel with
-                query: {
-                    presets: ['es2015'],
-                    plugins: ['transform-object-rest-spread']
-                }
-            },
-        ]
-    },
-    entry: {
-        "cpanel": ["./src/control-panel.js"]
-    },
-    output: {
-        path: path.resolve(__dirname, "public"),
-        publicPath: "/assets/",
-        filename: "[name].bundle.js"
-    },
-    devServer: { inline: true },
-    devtool: 'source-map',
-}
+        // Options to configure babel with
+        query: {
+          presets: ["es2015"],
+          plugins: ["transform-object-rest-spread"]
+        }
+      }
+    ]
+  },
+  entry: {
+    cpanel: ["./src/control-panel.js"],
+    tasks: ["./src/tasks.js"]
+  },
+  output: {
+    path: path.resolve(__dirname, "public"),
+    publicPath: "/assets/",
+    filename: "[name].bundle.js"
+  },
+  devServer: { inline: true },
+  devtool: "source-map"
+};
